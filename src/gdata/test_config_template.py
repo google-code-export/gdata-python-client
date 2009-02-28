3154d38657db704d6649c959e8e06a0195bd5bb3
j s <j.s@google.com>
2009-02-28 02:20:48 +0000

    The test config now reads the Blogger credentials using a function, so changes to module level constants will be propagated.

--- src/gdata/test_config_template.py
+++ src/gdata/test_config_template.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 # Copyright (C) 2009 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,24 +15,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 """Fill in this module with configuration data to use in tests.
 
 See comments in the source code for explanations of the settings.
 """
 
+
 # To actually run the tests which use this configuration information you must
 # change RUN_LIVE_TESTS to True.
 RUN_LIVE_TESTS = False
+
 
 # If set to True, the client will save responses from the server and reuse
 # them in future runs of the test.
 CACHE_RESPONSES = True
 
+
 GOOGLE_ACCOUNT_EMAIL = '<your email>'
 GOOGLE_ACCOUNT_PASSWORD = '<your password>'
 
-BLOGGER_CONFIG = {'email': GOOGLE_ACCOUNT_EMAIL, 
-                  'password': GOOGLE_ACCOUNT_PASSWORD, 
-                  'title': 'A Test Post',
-                  'content': 'This is a <b>test</b>.'
+
+def blogger_email():
+  """Provides email to log into the test Blogger account.
+  
+  By default uses GOOGLE_ACCOUNT_EMAIL, so edit this function if you have
+  a Blogger-specific test account.
+  """
+  return GOOGLE_ACCOUNT_EMAIL
+
+
+def blogger_password():
+  """Provides password to log into the test Blogger account.
+  
+  By default uses GOOGLE_ACCOUNT_PASSWORD, so edit this function if you have
+  a Blogger-specific test account.
+  """
+  return GOOGLE_ACCOUNT_PASSWORD
+
+
+BLOGGER_CONFIG = {'title': 'A Test Post',
+                  'content': 'This is a <b>test</b>.',
+                  'blog_id': '<your test blog\'s id>',
+                  'auth_token': None,
                   }
